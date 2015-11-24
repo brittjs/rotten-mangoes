028f1c390673d6543040da09e73efd94fde1ce9a
Brittany See <brittanyjsee@gmail.com>
2015-11-24 07:59:38 +0000

    implementing helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,23 @@
 class User < ActiveRecord::Base
 
-has_many :reviews
+  has_many :reviews
 
-has_secure_password
+  has_secure_password
 
-validates :email,
-  presence: true
+  validates :email,
+    presence: true
 
-validates :firstname,
-  presence: true
+  validates :firstname,
+    presence: true
 
-validates :lastname,
-  presence: true
+  validates :lastname,
+    presence: true
 
-validates :password,
-  length: { in: 6..20 }, on: :create
+  validates :password,
+    length: { in: 6..20 }, on: :create
+
+  def full_name
+    "#{firstname} #{lastname}"
+  end
 
 end
